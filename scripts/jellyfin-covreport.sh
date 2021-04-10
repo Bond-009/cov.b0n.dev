e70a719c638509087539b3ba0dba0b61d65519ca
Bond_009 <bond.009@outlook.com>
2021-04-10 16:04:07 +0200

    First

---
+++ scripts/jellyfin-covreport.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+cd "$1"
+
+find . -type d -name 'TestResults' -exec rm -r {} +
+dotnet test --configuration Release --collect:'XPlat Code Coverage' --settings tests/coverletArgs.runsettings --verbosity minimal
+reportgenerator "-title:Jellyfin" "-reporttypes:Html;Badges;JsonSummary" "-reports:tests/**/coverage.cobertura.xml" "-targetdir:$2" "-historydir:$3" "-verbosity:Warning"
