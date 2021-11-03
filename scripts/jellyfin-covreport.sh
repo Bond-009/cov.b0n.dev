9ab7b8f0557822eb6c5f1f8bb6a7c1db446ae4dc
Bond-009 <bond.009@outlook.com>
2021-11-03 14:39:19 +0100

    Ignore test projects

--- scripts/jellyfin-covreport.sh
+++ scripts/jellyfin-covreport.sh
@@ -6,4 +6,4 @@ cd "$1"
 
 find . -type d -name 'TestResults' -exec rm -r {} +
 dotnet test --configuration Release --collect:'XPlat Code Coverage' --settings tests/coverletArgs.runsettings --verbosity minimal
-reportgenerator "-title:Jellyfin" "-reporttypes:Html;Badges;JsonSummary" "-reports:tests/**/coverage.cobertura.xml" "-targetdir:$2" "-historydir:$3" "-verbosity:Warning"
+reportgenerator "-title:Jellyfin" "-reporttypes:Html;Badges;JsonSummary" "-reports:tests/**/coverage.cobertura.xml" "-assemblyfilters:-*.Tests" "-targetdir:$2" "-historydir:$3" "-verbosity:Warning"
